004442b0da455833605ee0019685c529ee85fa03
Leonardo Ferreira <leoventura245@gmail.com>
2025-01-26 19:10:01 -0300

    starting

--- TaskManagement/src/main/java/com/example/TaskManagement/TaskManagementApplication.java
+++ TaskManagement/src/main/java/com/example/TaskManagement/TaskManagementApplication.java
@@ -1,9 +1,15 @@
 package com.example.TaskManagement;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@SpringBootApplication // framework vai funcionando atraves dessas anotacoes
+@ Configuration // metodos que definem beans
+@ EnableAutoConfiguration // spring boot vai configurar automaticamente
+@ ComponentScan // permitir que o spring escaneie os componentes da aplicação
 public class TaskManagementApplication {
 
 	public static void main(String[] args) {
