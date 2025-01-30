bf83577b74bedb11adbf58071f4b8ff09bd4b36b
Leonardo Ferreira <leoventura245@gmail.com>
2025-01-30 00:37:20 -0300

    backend done

--- TaskManagement/src/main/java/com/example/TaskManagement/controllers/TaskController.java
+++ TaskManagement/src/main/java/com/example/TaskManagement/controllers/TaskController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController // Define que essa classe é um controlador REST
+@RestController // Define que essa classe é um controlador REST. Uma juncao de @Controller e @ResponseBody
 @RequestMapping("/tasks") // Define a URL base
 public class TaskController {
 
---
+++ TaskManagement/src/main/java/com/example/TaskManagement/models/TaskStatus.java
@@ -0,0 +1,7 @@
+package com.example.TaskManagement.models;
+
+public enum TaskStatus {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED
+}
